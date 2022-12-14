ab412730fa0795c6ad155fdf5b4a5c43de124c52
Dinusha <dinushamahagamage3403@gmail.com>
2022-12-14 16:46:06 +0530

    changed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0 };
+  state = {
+    count: 0,
+    imgeUrl: "https://picsum.photos/200",
+  };
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>counter</button>
+        <h1>Sagara Driving School</h1>
+        <img src={this.state.imgeUrl} alt="" />
+        <span class="badge badge-success m-2">{this.formatCount()}</span>
+        <span class="badge badge-pill badge-secondary">Danger</span>
+        <a href="https://picsum.photos/200" class="badge badge-primary">
+          Primary
+        </a>
+        <button type="button" class="btn btn-success">
+          counter
+        </button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
