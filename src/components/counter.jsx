9a48aee2cd918bf87c195741d6380c37ca11b6f3
Dinusha <dinushamahagamage3403@gmail.com>
2022-12-15 16:41:45 +0530

    change

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,39 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 4,
+    tags: ["tag1", "tag2", "tag3"],
     imgeUrl: "https://picsum.photos/200",
   };
+
+  styles = {
+    fontSize: 25,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
-      <React.Fragment>
+      <div>
         <h1>Sagara Driving School</h1>
         <img src={this.state.imgeUrl} alt="" />
-        <span>{this.formatCount()}</span>
+        <span style={this.styles}>{this.formatCount()}</span>
 
-        <button type="button" class="btn btn-success">
+        <button type="button" className={this.getButtonClasses()}>
           counter
         </button>
-      </React.Fragment>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
     );
+  }
+
+  getButtonClasses() {
+    let classes = "btn btn-sm btn-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
